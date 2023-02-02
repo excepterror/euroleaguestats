2e8ac18918303eec800f1cd275429193e4deac15
excepterror <thomas.sfondylis@gmail.com>
2023-02-02 21:15:06 +0200

    Minor changes.

--- Widgets/rv_stats.py
+++ Widgets/rv_stats.py
@@ -1,30 +1,4 @@
-from kivy.lang import Builder
 from kivy.uix.recycleview import RecycleView
-
-Builder.load_string('''
-<RV>:
-    viewclass: 'StatisticsLabel'
-    RecycleBoxLayout:
-        default_size: None, dp(46)
-        default_size_hint: 1, None
-        size_hint_y: None
-        height: self.minimum_height
-        size_hint_x: None
-        width: root.width
-        orientation: 'vertical'
-        bar_pos_y: 'right'
-        bar_color: .2, .6, .8, 1
-        bar_margin: -dp(6)
-        bar_width: dp(2)
-
-<StatisticsLabel@Label>:
-    text_size: self.width, None 
-    font_name: 'OpenSans' 
-    font_size: dp(16)        
-    halign: 'center'
-    valign: 'middle'
-    markup: True
-''')
 
 
 class RV(RecycleView):
