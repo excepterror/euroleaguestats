e95922b174e4626034a0abcb989a28d33f148a7b
excepterror <thomas.sfondylis@gmail.com>
2023-03-21 23:12:11 +0200

    Add arrows to sliding labels to improve user's experience.

--- Widgets/grid_for_roster.py
+++ Widgets/grid_for_roster.py
@@ -21,11 +21,11 @@ class SlidingLabel(RelativeLayout):
     name = StringProperty()
     scatter_ = ObjectProperty(None)
     label = ObjectProperty(None)
-    canvas_alpha = NumericProperty(0)
+    r_arrow = ObjectProperty(None)
+    l_arrow = ObjectProperty(None)
 
     def on_touch_up(self, touch):
         anim = Animation(x=0, duration=.5)
-        self.canvas_alpha = 0
         if self.collide_point(*touch.pos):
             self.scatter_.do_translation_x = True
             if self.scatter_.pos[0] >= self.width / 4:
@@ -46,10 +46,16 @@ class SlidingLabel(RelativeLayout):
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
-            self.canvas_alpha = .8
             self.parent.name = self.label.text
             self.parent.repeated_selection_flag += 1
+            if touch.x <= self.width * .5:
+                self.l_arrow.opacity = 1
+                anim_1 = Animation(x=self.width * .1, opacity=0, transition='in_out_quad', duration=.8) \
+                    + Animation(x=0, transition='in_out_quad', duration=.05)
+                anim_1.start(self.l_arrow)
+            elif touch.x >= self.width * .5:
+                self.r_arrow.opacity = 1
+                anim_2 = Animation(x=self.width * .8, opacity=0, transition='in_out_quad', duration=.8) \
+                    + Animation(x=self.width - self.r_arrow.width, transition='in_out_quad', duration=.05)
+                anim_2.start(self.r_arrow)
             return super().on_touch_down(touch)
-
-    def restore_canvas_color(self, *args):
-        self.label.color_canvas = (1, 1, 1, .8)
