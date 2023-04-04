63a1a85c0bfddddf6488015fd6dd38c04e941a1f
excepterror <thomas.sfondylis@gmail.com>
2023-04-04 22:05:07 +0300

    Bump version to 23.04.0.

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from Py.webview import WebViewInModal
 from Widgets.popups import MessagePopup, DisplayStats, NotesPopup
 from Widgets.rv_stats import RV
 
-__version__ = '23.03.4'
+__version__ = '23.03.5'
 
 
 class StatsByGame(Screen):
