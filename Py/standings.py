33f1e5032522c6b79870a11a56546d24a7229d6b
excepterror <thomas.sfondylis@gmail.com>
2023-12-08 21:52:14 +0200

    Patch code to pass html code for xpath from global_values.py.

--- Py/standings.py
+++ Py/standings.py
@@ -6,10 +6,20 @@ logging.getLogger().setLevel(logging.INFO)
 
 
 def fetch_standings():
+    try:
+        import global_values
+    except ModuleNotFoundError as error:
+        logging.warning('globals.py is missing: {}'.format(error))
+
     url = 'https://www.euroleaguebasketball.net/euroleague/standings/'
     standings = dict()
     try:
+        g1 = global_values.G1
+        g2 = global_values.G2
+        g3 = global_values.G3
         response = requests.get(url)
+    except NameError as error:
+        logging.warning('g1, g2, 33 values are not defined: {}'.format(error))
     except requests.exceptions.HTTPError as http_error:
         logging.warning('HTTP error occurred: {}'.format(http_error))
     except requests.exceptions.Timeout as timeout:
@@ -23,30 +33,17 @@ def fetch_standings():
         num_of_total_stat_cats = 11
         tree = etree.HTML(response.content)
 
-        teams_and_ranking = tree.xpath(
-            '//div[@class="complex-stat-table_body__UI0Lj"]'
-            '//div[@class="complex-stat-table_row__Jiu1w complex-stat-table__standingRow__KJaSV complex-stat-table__bottom8__h6iNh"]'
-            '//div[@class="complex-stat-table_sticky__TsX4D"]'
-            '//span[@class="complex-stat-table_mainClubName__1CeH5" or @class="complex-stat-table_mainClubName__1CeH5 complex-stat-table__long__YuBt_"]'
-            '/text()')
+        teams_and_ranking = tree.xpath(g1)
         for item in teams_and_ranking:
             if len(item) == 1:
                 teams_and_ranking.remove(item)
 
-        ranking = tree.xpath(
-            '//div[@class="complex-stat-table_body__UI0Lj"]'
-            '//div[@class="complex-stat-table_row__Jiu1w complex-stat-table__standingRow__KJaSV complex-stat-table__bottom8__h6iNh"]'
-            '//div[@class="complex-stat-table_sticky__TsX4D"]'
-            '//div[@class="complex-stat-table_cell__yQmD8 complex-stat-table__right__7D_Wn"]'
-            '//span/text()')
+        ranking = tree.xpath(g2)
         while a < len(teams_and_ranking):
             listing.append((ranking[a], teams_and_ranking[a]))
             a += 1
 
-        raw_data = tree.xpath(
-            '//div[@class="complex-stat-table_body__UI0Lj"]'
-            '//div[@class="complex-stat-table_row__Jiu1w complex-stat-table__standingRow__KJaSV complex-stat-table__bottom8__h6iNh"]'
-            '//div[@class="complex-stat-table_cell__yQmD8"]/text()')
+        raw_data = tree.xpath(g3)
 
         for item in listing:
             if item[0] in standings:
