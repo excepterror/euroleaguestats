63fd1132ca342c9143698a9abef9b5a833fd67a8
excepterror <thomas.sfondylis@gmail.com>
2023-12-29 20:45:18 +0200

    Differentiate colours for rankings 1-6 and 7-10.

--- Widgets/rv_standings.py
+++ Widgets/rv_standings.py
@@ -22,7 +22,21 @@ class RVSt(RecycleView):
 
     def on_current_standings(self, *args):
         teams_data = [
-            {'team_image': team_image, 'ranking': group[0], 'games_played': group[1][0], 'wins': group[1][1],
-             'losses': group[1][2], 'wins_percentage': group[1][3], 'points_plus': group[1][4], 'points_minus': group[1][5],
-             'points_diff': group[1][6]} for team_image, group in self.current_standings.items()]
+            {'team_image': team_image, 'ranking': '[color=FF6600]' + group[0] + '[/color]', 'games_played': group[1][0],
+             'wins': group[1][1],
+             'losses': group[1][2], 'wins_percentage': group[1][3], 'points_plus': group[1][4],
+             'points_minus': group[1][5],
+             'points_diff': group[1][6]}
+            if int(group[0]) in range(1, 7) else
+            {'team_image': team_image, 'ranking': '[color=#E69138]' + group[0] + '[/color]', 'games_played': group[1][0], 'wins': group[1][1],
+             'losses': group[1][2], 'wins_percentage': group[1][3], 'points_plus': group[1][4],
+             'points_minus': group[1][5],
+             'points_diff': group[1][6]}
+            if int(group[0]) in range(7, 11) else
+            {'team_image': team_image, 'ranking': group[0], 'games_played': group[1][0],
+             'wins': group[1][1],
+             'losses': group[1][2], 'wins_percentage': group[1][3], 'points_plus': group[1][4],
+             'points_minus': group[1][5],
+             'points_diff': group[1][6]}
+            for team_image, group in self.current_standings.items()]
         self.data = teams_data
