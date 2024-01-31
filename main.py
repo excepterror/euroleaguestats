e059cf01e9d4082d538d4badaadf72001a79d9ce
excepterror <thomas.sfondylis@gmail.com>
2024-01-31 22:42:17 +0200

    Changes to code to reflect consolidation of stats by game, average and total stat, all in :screen: Stats. Bump version to 24.1.1.

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import os
-import requests
-import concurrent.futures
 import logging
 import json
 
 from functools import partial
-from lxml import etree
-from requests.adapters import HTTPAdapter
-from urllib3.util import Retry
-from pathlib import Path
+
 from android.permissions import request_permissions, Permission
 
 from kivy.utils import platform
@@ -23,151 +18,48 @@ from kivy.metrics import sp
 
 from Py.connectivity import connectivity_status
 from Py.standings import fetch_standings
-from Py.stats import update_dict, access_per_game_stats
+from Py.extract_bio_stats import extract_players_data
+from Py.fetch_trees import fetch_trees
+from Py.extract_game_stats import update_dict
 from Py.webview import WebViewInModal
 
-from Widgets.popups import MessagePopup, DisplayStats, NotesPopup
+from Widgets.popups import MessagePopup, NotesPopup, DisplayStats
 from Widgets.rv_stats import RV
 
-__version__ = '23.12.3'
-
-
-class StatsByGame(Screen):
-    player_name = StringProperty('')
-    player_tree = DictProperty({})
-    recycle_view_mod = ObjectProperty(None)
-    data = ListProperty([])
-    opponents = ObjectProperty(None)
-    message = ObjectProperty(None)
-    notification = StringProperty('')
-
-    def extract_player_games(self):
-        self.opponents = self.player_tree.xpath('//div[@class="stats-table_table__dpgY7"]')
-        if len(self.opponents) != 0:
-            data = access_per_game_stats(self.player_tree, self.player_name)
-            self.data = data
-        else:
-            text = 'No games played by ' + self.player_name + ' yet!'
-            self.notification = text
-
-    def on_data(self, *args):
-        self.recycle_view_mod.perf_data = self.data
-
-    def on_notification(self, *args):
-        if self.notification != '':
-            self.message = MessagePopup(on_open=self.dismiss_text)
-            self.message.notification.text = self.notification
-            self.message.open()
-
-    def dismiss_text(self, *args):
-        Clock.schedule_once(self.message.dismiss, 1.5)
-        Clock.schedule_once(self.reset_text, 1.6)
-
-    def reset_text(self, *args):
-        self.notification = ''
-
-    @staticmethod
-    def call_teams_screen(*args):
-        del App.get_running_app().root.screens_visited[1:]
-        App.get_running_app().root.transition = FadeTransition(duration=.5)
-        App.get_running_app().root.current = 'teams'
+__version__ = '24.1.0'
 
 
 class Stats(Screen):
-    player_tree = DictProperty({})
-    player_name = StringProperty('')
-    player_url = StringProperty()
-    player_photo = StringProperty('Images/NoImage.jpg')
+    player_tree_data = ListProperty([])
+    recycle_view_mod = ObjectProperty(None)
+    notification = StringProperty('')
+    message = ObjectProperty(None)
     text_1 = StringProperty('')
     text_2 = StringProperty('')
-    stats = ListProperty([])
+    player_name = StringProperty('')
+    player_photo = StringProperty('')
+    display_stats = ObjectProperty(None)
     rv = ObjectProperty(None)
-    show_stats = ObjectProperty(None)
-    message = ObjectProperty(None)
-    notification = StringProperty('')
+    display_stats_title = StringProperty('')
 
-    def extract_players_data(self):
-        pos = self.player_tree.xpath(
-            '//div[@class="player-hero_inner__rwwR_ side-gaps_sectionSideGaps__v5CKj"]'
-            '//div[@class="hero-info_position__GDXbP"]/text()')
-        info_1 = self.player_tree.xpath(
-            '//div[@class="player-hero_inner__rwwR_ side-gaps_sectionSideGaps__v5CKj"]'
-            '//ul[@class="hero-info_dataList__kKi0z"]//li[@class="hero-info_dataItem__UbJZU"]'
-            '//span[@class="hero-info_key__Pcrzp"]/text()')
-        info_2 = self.player_tree.xpath(
-            '//div[@class="player-hero_inner__rwwR_ side-gaps_sectionSideGaps__v5CKj"]'
-            '//ul[@class="hero-info_dataList__kKi0z"]//li[@class="hero-info_dataItem__UbJZU"]'
-            '//b[@class="hero-info_value__XFJeE"]/text()')
-
-        info = list()
-        for i, j, in zip(info_1, info_2):
-            s = i + ': ' + j
-            info.append(s)
-
-        try:
-            '''Check if photo already exists.'''
-            if self.player_photo.strip('.png') == self.player_name:
-                pass
-            elif len(self.player_url) != 0:
-                try:
-                    session = requests.Session()
-                    retry = Retry(connect=3, backoff_factor=0.5)
-                    adapter = HTTPAdapter(max_retries=retry)
-                    session.mount('http://', adapter)
-                    session.mount('https://', adapter)
-                    response = session.get(self.player_url)
-                    player_photo = self.player_name + '.png'
-                    with open(player_photo, 'wb') as f:
-                        f.write(response.content)
-                    self.player_photo = self.player_name + '.png'
-                except requests.exceptions.ConnectTimeout as conn_timeout:
-                    logging.warning('Connection timed-out: {}'.format(conn_timeout))
-                except Exception as e:
-                    logging.warning(e)
-            else:
-                self.player_photo = 'Images/NoImage.jpg'
-        except requests.exceptions.RequestException as request_exceptions:
-            logging.warning('Requests exceptions occurred: {}'.format(request_exceptions))
-
-        try:
-            self.text_1 = pos[0]
-            self.text_2 = '\n' + info[0] + '\n\n' + info[1] + '\n\n' + info[2]
-        except IndexError as index_error:
-            logging.warning('Index error occurred: {}'.format(index_error))
-
-    def select_stats(self, instance, *args):
-        if instance.text == 'Average Stats':
-            average_stats = self.player_tree.xpath(
-                '//div[@class="tab-season_seasonTableWrap__I0CUd"]//div[@class="stats-table_table__dpgY7"]'
-                '//div[@class="stats-table_row__ttfiG"][3]//div[@class="stats-table_cell__hdmqc"]/text()')
-            if len(average_stats) != 0:
-                self.show_stats = DisplayStats()
-                self.show_stats.title = 'Average Stats for ' + self.player_name
-                self.rv = RV(update_dict(average_stats))
-            else:
-                text = 'No games played by ' + self.player_name + ' yet!'
-                self.notification = text
-        elif instance.text == 'Total Stats':
-            total_stats = self.player_tree.xpath(
-                '//div[@class="tab-season_seasonTableWrap__I0CUd"]//div[@class="stats-table_table__dpgY7"]'
-                '//div[@class="stats-table_row__ttfiG"][2]//div[@class="stats-table_cell__hdmqc"]/text()')
-            if len(total_stats) != 0:
-                self.show_stats = DisplayStats()
-                self.show_stats.title = 'Total Stats for ' + self.player_name
-                self.rv = RV(update_dict(total_stats))
-            else:
-                text = 'No games played by ' + self.player_name + ' yet!'
-                self.notification = text
-
-    def on_rv(self, *args):
-        self.show_stats.content = self.rv
-        self.show_stats.open()
-
-    def on_notification(self, *args):
+    def on_player_tree_data(self, *args):
+        self.notification = self.player_tree_data[6]
         if self.notification != '':
-            self.message = MessagePopup(on_open=self.dismiss_text)
-            self.message.notification.text = self.notification
-            self.message.open()
+            self.open_popup()
+        else:
+            self.recycle_view_mod.perf_data = self.player_tree_data[5]
+            self.text_1, self.text_2, self.player_photo = [self.player_tree_data[i] for i in range(3)]
+            self.call_this_screen()
+
+    @staticmethod
+    def call_this_screen(*args):
+        App.get_running_app().root.transition = FadeTransition(duration=.5)
+        App.get_running_app().root.current = 'stats'
+
+    def open_popup(self, *args):
+        self.message = MessagePopup(on_open=self.dismiss_text)
+        self.message.notification.text = self.notification
+        self.message.open()
 
     def dismiss_text(self, *args):
         Clock.schedule_once(self.message.dismiss, 1.5)
@@ -176,17 +68,36 @@ class Stats(Screen):
     def reset_text(self, *args):
         self.notification = ''
 
-    def stats_animate_on_push(self, instance):
-        anim = Animation(size_hint_x=.68, duration=.2)
-        anim &= Animation(height=sp(70), duration=.2)
+    def animate_on_push(self, instance, *args):
+        anim = Animation(size_hint_x=.45, font_size=sp(17), duration=.2)
+        anim &= Animation(height=sp(40), duration=.2)
         anim.start(instance)
         anim.on_complete(Clock.schedule_once(partial(self.stats_reverse_animate, instance), .2))
 
     def stats_reverse_animate(self, instance, *args):
-        anim = Animation(size_hint_x=.7, duration=.1)
-        anim &= Animation(height=sp(80), duration=.1)
+        anim = Animation(size_hint_x=.47, font_size=sp(20), duration=.1)
+        anim &= Animation(height=sp(50), duration=.1)
         anim.start(instance)
-        anim.on_complete(Clock.schedule_once(partial(self.select_stats, instance), .2))
+        anim.on_complete(Clock.schedule_once(partial(self.show_stats, instance), .2))
+
+    def show_stats(self, instance, *args):
+        if instance.text == 'Average Stats':
+            self.display_stats = DisplayStats()
+            self.display_stats_title = instance.text + ' for ' + self.player_name
+            self.rv = RV(update_dict(self.player_tree_data[3]))
+        elif instance.text == 'Total Stats':
+            self.display_stats = DisplayStats()
+            self.display_stats_title = instance.text + ' for ' + self.player_name
+            self.rv = RV(update_dict(self.player_tree_data[4]))
+
+    def on_rv(self, *args):
+        self.display_stats.content = self.rv
+        self.display_stats.title = self.display_stats_title
+        self.display_stats.open()
+
+    def reset_rv(self, *args):
+        print('ok')
+        self.clear_widgets()
 
     @staticmethod
     def call_teams_screen(*args):
@@ -198,67 +109,22 @@ class Stats(Screen):
 class Roster(Screen):
     grid_roster = ObjectProperty(None)
     trees = DictProperty({})
-    player_name = StringProperty()
+    player_name = StringProperty('')
     repeated_selection_flag = NumericProperty(0)
     canvas_opacity = NumericProperty(0)
+    assert_tree_return = ListProperty([])
+    notification = StringProperty('')
+    message = ObjectProperty(None)
+
+    def extract_trees(self, *args):
+        self.trees = fetch_trees(self.grid_roster)
 
     def assert_tree(self, *args):
         for name, t in self.trees.items():
             if name == self.player_name:
-                Stats.player_tree = self.trees[name][0]
-                Stats.player_url = self.trees[name][1]
-                StatsByGame.player_tree = self.trees[name][0]
-                StatsByGame.player_name = name
-
-    @staticmethod
-    def fetch_tree(url):
-        session = requests.Session()
-        retry = Retry(connect=3, backoff_factor=0.5)
-        adapter = HTTPAdapter(max_retries=retry)
-        session.mount('http://', adapter)
-        session.mount('https://', adapter)
-        response = session.get(url)
-        t = etree.HTML(response.content)
-        return t
-
-    def fetch_trees(self, *args):
-        temp_dict = dict()
-        roster_keys = self.grid_roster.roster.keys()
-        roster_values = self.grid_roster.roster.values()
-
-        names = list(roster_keys)
-        roster_values = list(roster_values)
-
-        players_urls = list()
-        photo_urls = list()
-        for _tuple in roster_values:
-            players_urls.append(_tuple[0])
-            photo_urls.append(_tuple[1])
-
-        try:
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                trees = executor.map(self.fetch_tree, players_urls)
-        except TimeoutError as timeout_error:
-            logging.warning('Timeout error occurred: {}'.format(timeout_error))
-
-        for name, tree, url in zip(names, trees, photo_urls):
-            if name in temp_dict:
-                temp_dict[name] = name
-            else:
-                temp_dict[name] = tree, url
-
-        self.trees = temp_dict
-        self.grid_roster.roster = dict()
-
-    def call_stats(self):
-        App.get_running_app().root.transition = SlideTransition(direction='left')
-        App.get_running_app().root.current = 'stats'
-        self.grid_roster.stats_option = False
-
-    def call_stats_by_game(self):
-        App.get_running_app().root.transition = SlideTransition(direction='left')
-        App.get_running_app().root.current = 'stats_by_game'
-        self.grid_roster.stats_by_game_option = False
+                player_tree = self.trees[name][0]
+                player_url = self.trees[name][1]
+                self.assert_tree_return = extract_players_data(player_tree, name, player_url)
 
     @staticmethod
     def call_teams_screen(*args):
@@ -391,33 +257,13 @@ class Home(Screen):
         self.rosters_reg = data
 
     def download_global_values_file(self, *args):
-        conn = connectivity_status()
-        if conn is True:
-            global_url = 'https://drive.google.com/uc?export=download&id=1_hiu_G8jm7LABNZevYMzJef4UfFM5H0L'
-            response = requests.get(global_url, params={"downloadformat": "txt"})
-            if response.status_code == 200:
-                with open('global_values.txt', mode='wb') as file:
-                    file.write(response.content)
-                path = Path('global_values.txt')
-                path.rename(path.with_suffix('.py'))
-            try:
-                import global_values
-            except ModuleNotFoundError as error:
-                logging.warning('globals.py is missing: {}'.format(error))
-            else:
-                pass
-            try:
-                json_url = global_values.JSON_URL
-                response = requests.get(json_url)
-                if response.status_code == 200:
-                    with open('roster.json', mode='wb') as file:
-                        file.write(response.content)
-                    self.create_dict_with_rosters()
-            except ValueError as value_error:
-                logging.warning('Value error occurred: {}'.format(value_error))
-                Clock.schedule_once(partial(self.time_out_popup, conn='Error while downloading data.'), 1)
+        try:
+            import global_values
+        except ModuleNotFoundError as error:
+            logging.warning('globals.py is missing: {}'.format(error))
         else:
-            Clock.schedule_once(partial(self.time_out_popup, conn), 1)
+            pass
+        self.create_dict_with_rosters()
 
     @staticmethod
     def time_out_popup(conn, *args):
@@ -486,12 +332,11 @@ class EuroLeagueStatsApp(App):
         return ELSScreenManager()
 
     def on_stop(self):
-        suffixes = ('.png', 'json')
+        suffixes = '.png'
         for file_name in os.listdir(os.getcwd()):
-            if file_name.endswith(suffixes) and file_name not in ('Court.jpg', 'NoImage.jpg'):
+            if file_name.endswith(suffixes):
                 try:
                     os.remove(file_name)
-                    os.remove('global_values.py')
                 except OSError as os_error:
                     logging.warning('OS error occurred: {}'.format(os_error))
 
