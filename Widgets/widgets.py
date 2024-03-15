48c360a7c2f8820d88cb273f5a3b3b3522e84b19
excepterror <thomas.sfondylis@gmail.com>
2024-03-16 00:03:40 +0200

    Add :cls: PlayerImage.

--- Widgets/widgets.py
+++ Widgets/widgets.py
@@ -1,5 +1,7 @@
 from kivy.uix.label import Label
 from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.image import Image
+from kivy.properties import StringProperty
 
 
 class RoundedRectLabelBtn(ButtonBehavior, Label):
@@ -12,3 +14,12 @@ class NullLabel(Label):
 
 class StandingsLabel(Label):
     pass
+
+
+class PlayerImage(Image):
+    player = StringProperty('')
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            self.parent.parent.parent.assert_tree(self.player)
+            return super().on_touch_down(touch)
