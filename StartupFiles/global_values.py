9ead84e67f4ae83fcc004815da53ca4fc8283c83
excepterror <thomas.sfondylis@gmail.com>
2024-11-08 17:46:45 +0200

    Update html code for new season 2024-25.

--- StartupFiles/global_values.py
+++ StartupFiles/global_values.py
@@ -1,50 +1,50 @@
 """Constant Variables in standings.py"""
 
-G1 = '//div[@class="complex-stat-table_body__UI0Lj"]' \
-     '//div[@class="complex-stat-table_row__Jiu1w complex-stat-table__standingRow__KJaSV complex-stat-table__euroLeagueNewSystem__jSHq9 complex-stat-table__euroleague__zV5Mu"]' \
-     '//div[@class="complex-stat-table_sticky__TsX4D"]' \
-     '//span[@class="complex-stat-table_mainClubName__1CeH5" or @class="complex-stat-table_mainClubName__1CeH5 complex-stat-table__long__YuBt_"]' \
+G1 = '//div[@class="complex-stat-table_body__UF_OT"]' \
+     '//div[@class="complex-stat-table_row__XPRhI complex-stat-table__standingRow__bPVTR complex-stat-table__euroLeagueNewSystem__Y6qYU complex-stat-table__euroleague__z1bG4"]' \
+     '//div[@class="complex-stat-table_sticky__KBwSE"]' \
+     '//span[@class="complex-stat-table_mainClubName__drf8e" or @class="complex-stat-table_mainClubName__drf8e complex-stat-table__long__BrSna"]' \
      '/text()'
 
-G2 = '//div[@class="complex-stat-table_body__UI0Lj"]' \
-     '//div[@class="complex-stat-table_row__Jiu1w complex-stat-table__standingRow__KJaSV complex-stat-table__euroLeagueNewSystem__jSHq9 complex-stat-table__euroleague__zV5Mu"]' \
-     '//div[@class="complex-stat-table_sticky__TsX4D"]' \
-     '//div[@class="complex-stat-table_cell__yQmD8 complex-stat-table__right__7D_Wn"]' \
+G2 = '//div[@class="complex-stat-table_body__UF_OT"]' \
+     '//div[@class="complex-stat-table_row__XPRhI complex-stat-table__standingRow__bPVTR complex-stat-table__euroLeagueNewSystem__Y6qYU complex-stat-table__euroleague__z1bG4"]' \
+     '//div[@class="complex-stat-table_sticky__KBwSE"]' \
+     '//div[@class="complex-stat-table_cell__XIEO5 complex-stat-table__right__ge6x9"]' \
      '//span/text()'
 
-G3 = '//div[@class="complex-stat-table_body__UI0Lj"]' \
-     '//div[@class="complex-stat-table_row__Jiu1w complex-stat-table__standingRow__KJaSV complex-stat-table__euroLeagueNewSystem__jSHq9 complex-stat-table__euroleague__zV5Mu"]' \
-     '//div[@class="complex-stat-table_cell__yQmD8"]/text()'
+G3 = '//div[@class="complex-stat-table_body__UF_OT"]' \
+     '//div[@class="complex-stat-table_row__XPRhI complex-stat-table__standingRow__bPVTR complex-stat-table__euroLeagueNewSystem__Y6qYU complex-stat-table__euroleague__z1bG4"]' \
+     '//div[@class="complex-stat-table_cell__XIEO5"]/text()'
 
 
 """Constant Variables in stats.py"""
 
-S_PREFIX = '//div[@class="stats-table_table__dpgY7"]//div[@class="stats-table_colGroup__hJ_6t"]'\
-         '//div[@class="stats-table_row__ttfiG stats-table__hasLink__N5MQe"]'
+S_PREFIX = '//div[@class="stats-table_table__cD0GH"]//div[@class="stats-table_colGroup__zvDOb"]'\
+         '//div[@class="stats-table_row__wEFis stats-table__hasLink__D8bNs"]'
 
-S1 = S_PREFIX + '//div[@class="stats-table_cell__hdmqc"][1]/text()'
+S1 = S_PREFIX + '//div[@class="stats-table_cell___AWMd"][1]/text()'
 
-S2 = S_PREFIX + '//div[@class="stats-table_cell__hdmqc"][2]/text()'
+S2 = S_PREFIX + '//div[@class="stats-table_cell___AWMd"][2]/text()'
 
-S3 = S_PREFIX + '//span[@class="stats-table_vs__jx4I1"]/text()'
+S3 = S_PREFIX + '//span[@class="stats-table_vs__xkbtN"]/text()'
 
-V_PREFIX = '//div[@class="tab-season_phaseTablesWrap__CijM6"]//div[@class="stats-table_table__dpgY7"]'\
-           '//div[@class="stats-table_main__zob_I"]'
+V_PREFIX = '//div[@class="tab-season_phaseTablesWrap__DK_Y8"]//div[@class="stats-table_table__cD0GH"]'\
+           '//div[@class="stats-table_main__ZIFxd"]'
 
 V1 = V_PREFIX + '//div[@data-key="Min-Pts-2FG-3FG-FT"]'\
-                '//div[@class="stats-table_row__ttfiG"]//div[@class="stats-table_cell__hdmqc"]/text()'
+                '//div[@class="stats-table_row__wEFis"]//div[@class="stats-table_cell___AWMd"]/text()'
 
 V2 = V_PREFIX + '//div[@data-key="O-D-T"]'\
-                '//div[@class="stats-table_row__ttfiG"]//div[@class="stats-table_cell__hdmqc"]/text()'
+                '//div[@class="stats-table_row__wEFis"]//div[@class="stats-table_cell___AWMd"]/text()'
 
 V3 = V_PREFIX + '//div[@data-key="As-St-To"]'\
-                '//div[@class="stats-table_row__ttfiG"]//div[@class="stats-table_cell__hdmqc"]/text()'
+                '//div[@class="stats-table_row__wEFis"]//div[@class="stats-table_cell___AWMd"]/text()'
 
 V4 = V_PREFIX + '//div[@data-key="Fv-Ag"]'\
-                '//div[@class="stats-table_row__ttfiG"]//div[@class="stats-table_cell__hdmqc"]/text()'
+                '//div[@class="stats-table_row__wEFis"]//div[@class="stats-table_cell___AWMd"]/text()'
 
 V5 = V_PREFIX + '//div[@data-key="Cm-Rv"]'\
-                '//div[@class="stats-table_row__ttfiG"]//div[@class="stats-table_cell__hdmqc"]/text()'
+                '//div[@class="stats-table_row__wEFis"]//div[@class="stats-table_cell___AWMd"]/text()'
 
 V6 = V_PREFIX + '//div[@data-key="PIR"]'\
-                '//div[@class="stats-table_row__ttfiG"]//div[@class="stats-table_cell__hdmqc"]/text()'
+                '//div[@class="stats-table_row__wEFis"]//div[@class="stats-table_cell___AWMd"]/text()'
