0bcac1b740012fe98072e001a8a55f4b83df286b
excepterror <thomas.sfondylis@gmail.com>
2025-04-04 20:47:29 +0300

    Code improvements.

--- View/MenuScreen/menu_screen.py
+++ View/MenuScreen/menu_screen.py
@@ -1,32 +1,33 @@
 from kivy.uix.screenmanager import Screen
 from kivy.animation import Animation
-from kivy.clock import Clock
 from kivy.app import App
-
-from functools import partial
+from kivy.properties import ObjectProperty
 
 
 class MenuScreenView(Screen):
+    teams = ObjectProperty(None)
+    standings = ObjectProperty(None)
+    about = ObjectProperty(None)
 
     def stats_animate_on_push(self, instance):
         anim = Animation(size_hint=[.86, .06], duration=.1)
+        anim.bind(on_complete=lambda *args: self.stats_reverse_animate(instance))
         anim.start(instance)
-        anim.on_complete(Clock.schedule_once(partial(self.stats_reverse_animate, instance), .1))
 
     def stats_reverse_animate(self, instance, *args):
         anim = Animation(size_hint=[.88, .08], duration=.1)
+        anim.bind(on_complete=lambda *args: self.screen_selection(instance))
         anim.start(instance)
-        anim.on_complete(Clock.schedule_once(partial(self.screen_selection, instance), .1))
 
     def about_animate_on_push(self, instance):
         anim = Animation(size_hint=[.38, .04], duration=.1)
+        anim.bind(on_complete=lambda *args: self.about_reverse_animate(instance))
         anim.start(instance)
-        anim.on_complete(Clock.schedule_once(partial(self.about_reverse_animate, instance), .1))
 
     def about_reverse_animate(self, instance, *args):
         anim = Animation(size_hint=[.4, .06], duration=.1)
+        anim.bind(on_complete=lambda *args: self.screen_selection(instance))
         anim.start(instance)
-        anim.on_complete(Clock.schedule_once(partial(self.screen_selection, instance), .1))
 
     def screen_selection(self, instance, *args):
         if instance is self.teams:
