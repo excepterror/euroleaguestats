232c26421318d7fe1da9b8bc8211338d5616e610
excepterror <thomas.sfondylis@gmail.com>
2025-10-02 01:30:22 +0300

    Adjust loading sequence to include the loading message. Add date specifics to let user know of data availability.

--- View/HomeScreen/home_screen.py
+++ View/HomeScreen/home_screen.py
@@ -9,6 +9,7 @@ from kivy.clock import Clock
 from functools import partial
 from datetime import datetime
 
+from Widgets.widgets import LoadingMessage
 
 class HomeScreenView(Screen):
     rosters_reg = DictProperty({})
@@ -17,7 +18,11 @@ class HomeScreenView(Screen):
     notification = ObjectProperty(None)
 
     def schedule_intro_image_display(self, *args):
-        Clock.schedule_once(self.import_global_values_file, 0)
+        Clock.schedule_once(self.play_loading_message, 0)
+        Clock.schedule_once(self.import_global_values_file, 2)
+
+    def play_loading_message(self, *args):
+        self.add_widget(LoadingMessage())
 
     def import_global_values_file(self, *args):
         try:
@@ -46,14 +51,20 @@ class HomeScreenView(Screen):
         app = App.get_running_app()
 
         _standings = fetch_standings()
+
+        today = datetime.today()
+        year = today.year
+        start_date = datetime(year, 6, 15)
+        end_date = datetime(year, 10, 15)
+
         if isinstance(_standings, str):
             self.critical_error_and_exit(message=_standings)
-        elif datetime.today().month in range(6, 10):
-            logging.info("[home_screen.py] Running the app after season, i.e. June onwards.")
+        elif start_date <= today <= end_date:
+            logging.info("[home_screen.py] Running the app post-season, i.e. June onwards.")
             self.standings = _standings
             app.load_kv_files()
             self.call_notification_popup(source="Assets/notification_important_24dp.png",
-                                         notification_content="The content is partially unavailable as Euroleague are still making changes to their website!",
+                                         notification_content="The content may be partially unavailable as Euroleague are still making changes to their website!",
                                          timeout=6)
             Clock.schedule_once(partial(app.set_current_screen, "menu screen"), 7)
         else:
@@ -67,15 +78,15 @@ class HomeScreenView(Screen):
         Clock.schedule_once(App.get_running_app().stop, exit_delay + 1)
 
     def data_from_dataset(self):
-        """Form data for RVStandings recycleview. Called by :def: set_current_screen in main.py."""
+        """Group data for RVStandings recycleview. Called by :def: set_current_screen in main.py."""
         dataset = [
-            {'team_image': team_image, 'ranking': '[color=FF6600]' + group[0] + '[/color]', 'games_played': group[1][0],
+            {'team_image': team_image, 'ranking': '[color=#000000]' + group[0] + '[/color]', 'games_played': group[1][0],
              'wins': group[1][1],
              'losses': group[1][2], 'wins_percentage': group[1][3], 'points_plus': group[1][4],
              'points_minus': group[1][5],
              'points_diff': group[1][6]}
             if int(group[0]) in range(1, 7) else
-            {'team_image': team_image, 'ranking': '[color=#E69138]' + group[0] + '[/color]', 'games_played': group[1][0], 'wins': group[1][1],
+            {'team_image': team_image, 'ranking': '[color=#28282B]' + group[0] + '[/color]', 'games_played': group[1][0], 'wins': group[1][1],
              'losses': group[1][2], 'wins_percentage': group[1][3], 'points_plus': group[1][4],
              'points_minus': group[1][5],
              'points_diff': group[1][6]}
