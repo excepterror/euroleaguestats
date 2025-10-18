956b29e6442fe9768d50eaff8d9d555a12b5622c
excepterror <thomas.sfondylis@gmail.com>
2025-10-18 11:01:11 +0300

    Code improvements.

--- View/WelcomeScreen/welcome_screen.py
+++ View/WelcomeScreen/welcome_screen.py
@@ -2,20 +2,18 @@ from kivy.uix.screenmanager import Screen
 from kivy.animation import Animation
 from kivy.clock import Clock
 
-from PyCoreFiles.webview import WebViewInModal
-
 
 class WelcomeScreenView(Screen):
 
     def stats_animate_on_push(self, instance):
         anim = Animation(size_hint=[.86, .06], duration=.1)
+        anim.bind(on_complete=lambda *a: self.stats_reverse_animate(instance))
         anim.start(instance)
-        anim.bind(on_complete=self.stats_reverse_animate(instance))
 
     def stats_reverse_animate(self, instance, *args):
         anim = Animation(size_hint=[.88, .08], duration=.05)
+        anim.bind(on_complete=lambda *a: self.selection(instance))
         anim.start(instance)
-        anim.bind(on_complete=self.selection(instance))
 
     def selection(self, instance, *args):
         if instance is self.privacy_policy:
@@ -23,5 +21,6 @@ class WelcomeScreenView(Screen):
 
     @staticmethod
     def view_privacy_policy(*args):
+        from PyCoreFiles.webview import WebViewInModal
         view = WebViewInModal()
         view.open_web_view()
