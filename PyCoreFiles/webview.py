b4e604ae58711eadff00ac61274e45fee82ff736
ExceptError <thomas.sfondylis@gmail.com>
2025-11-17 23:10:42 +0200

    Update webview.py

--- PyCoreFiles/webview.py
+++ PyCoreFiles/webview.py
@@ -18,10 +18,12 @@ class KeyListener(PythonJavaClass):
 
     @java_method('(Landroid/view/View;ILandroid/view/KeyEvent;)Z')
     def onKey(self, v, key_code, event):
-        key_event = autoclass('android.view.KeyEvent')
-        if event.getAction() == key_event.ACTION_DOWN and key_code == key_event.KEYCODE_BACK:
+        KeyEvent = autoclass('android.view.KeyEvent')
+        # Only on ACTION_DOWN and BACK key do our custom action
+        if event.getAction() == KeyEvent.ACTION_DOWN and key_code == KeyEvent.KEYCODE_BACK:
             return self.listener()
-        return True
+        # Return False to allow propagation when not handled (optional)
+        return False
 
 
 class WebViewInModal(ModalView):
@@ -29,34 +31,42 @@ class WebViewInModal(ModalView):
     webview = ObjectProperty(None)
     layout = ObjectProperty(None)
 
+    # Keep strong references here
+    _key_listener = None
+
     def open_web_view(self):
         self.open()
 
     @run_on_ui_thread
     def on_open(self):
-        web_view = autoclass('android.webkit.WebView')
-        web_view_client = autoclass('android.webkit.WebViewClient')
-        python_activity = autoclass('org.kivy.android.PythonActivity')
-        layout_params = autoclass('android.view.ViewGroup$LayoutParams')
-        linear_layout = autoclass('android.widget.LinearLayout')
-        color = autoclass('android.graphics.Color')
+        WebView = autoclass('android.webkit.WebView')
+        WebViewClient = autoclass('android.webkit.WebViewClient')
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+        LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
+        LinearLayout = autoclass('android.widget.LinearLayout')
+        Color = autoclass('android.graphics.Color')
 
-        activity = python_activity.mActivity
-        webview = web_view(activity)
-        webview.setWebViewClient(web_view_client())
-        webview.setBackgroundColor(color.BLACK)
+        activity = PythonActivity.mActivity
+        webview = WebView(activity)
+        webview.setWebViewClient(WebViewClient())
+        webview.setBackgroundColor(Color.BLACK)
         webview.getSettings().setJavaScriptEnabled(True)
         webview.getSettings().setBuiltInZoomControls(True)
         webview.getSettings().setDisplayZoomControls(False)
 
-        layout = linear_layout(activity)
-        layout.setOrientation(linear_layout.VERTICAL)
-        layout.addView(webview, layout_params(-1, -1))
-        activity.addContentView(layout, layout_params(-1, -1))
+        layout = LinearLayout(activity)
+        layout.setOrientation(LinearLayout.VERTICAL)
+        layout.addView(webview, LayoutParams(-1, -1))
+        activity.addContentView(layout, LayoutParams(-1, -1))
 
-        webview.setOnKeyListener(KeyListener(self._back_pressed))
-        self.webview = web_view
+        # keep a strong reference to the listener to avoid GC + crash
+        self._key_listener = KeyListener(self._back_pressed)
+        webview.setOnKeyListener(self._key_listener)
+
+        # assign the instance (not the autoclass)
+        self.webview = webview
         self.layout = layout
+
         try:
             webview.loadUrl('https://github.com/excepterror/euroleaguestats/blob/master/PRIVACY%20POLICY.md')
         except Exception as e:
@@ -67,8 +77,8 @@ class WebViewInModal(ModalView):
     def on_size(self, instance, size):
         if self.webview:
             params = self.webview.getLayoutParams()
-            params.width = self.width
-            params.height = self.height
+            params.width = int(self.width)
+            params.height = int(self.height)
             self.webview.setLayoutParams(params)
 
     def pause(self):
@@ -89,14 +99,29 @@ class WebViewInModal(ModalView):
             parent = cast(view_group, self.layout.getParent())
             if parent is not None:
                 parent.removeView(self.layout)
-            self.webview.clearHistory()
-            self.webview.clearCache(True)
-            self.webview.clearFormData()
-            self.webview.destroy()
-            self.webview = None
+
+            # cleanup listener (unset) and webview properly
+            if self.webview:
+                try:
+                    # Remove listener to avoid further Java callbacks
+                    self.webview.setOnKeyListener(None)
+                except Exception:
+                    pass
+                try:
+                    self.webview.clearHistory()
+                    self.webview.clearCache(True)
+                    self.webview.clearFormData()
+                    self.webview.destroy()
+                except Exception:
+                    pass
+                finally:
+                    self.webview = None
+
+            # drop our strong reference
+            self._key_listener = None
 
     def _back_pressed(self):
-        if self.webview.canGoBack():
+        if self.webview and self.webview.canGoBack():
             self.webview.goBack()
         else:
             self.dismiss()
